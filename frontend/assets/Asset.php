1e2172f4f37492a1aff477fb183636df1c1430b6
sahakgagikian <sgagikyan@gmail.com>
2022-03-22 01:20:16 +0400

    jh-3 integrated new assets and layout

---
+++ frontend/assets/Asset.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace frontend\assets;
+
+/**
+ * Main frontend application asset bundle.
+ */
+class Asset extends AppAsset
+{
+    public $css = [
+        'css/bootstrap.min.css',
+        'css/line-icons.css',
+        'css/owl.carousel.min.css',
+        'css/owl.theme.default.css',
+        'css/slicknav.min.css',
+        'css/animate.css',
+        'css/main.css',
+        'css/responsive.css'
+    ];
+    public $js = [
+        'js/popper.min.js',
+        'js/bootstrap.min.js',
+        'js/color-switcher.js',
+        'js/owl.carousel.min.js',
+        'js/jquery.slicknav.js',
+        'js/jquery.counterup.min.js',
+        'js/waypoints.min.js',
+        'js/form-validator.min.js',
+        'js/contact-form-script.js',
+        'js/main.js',
+        'js/addons.js',
+        'js/prevent-refresh-on-validation-fail.js',
+    ];
+}
